bf5a38156682d329f7603451b7c224f956807605
lucasmarinn <lucasmarin27@gmail.com>
2023-12-29 17:13:11 -0300

    Codigo encerrado

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Ola mundo!");
+let nome = "Lucas"
+let xp = 8500
+let classificacao;
+
+if (xp<= 1000){
+    classificacao = 'Ferro';
+} else if (xp <=2000){
+    classificacao = 'Bronze';
+} else if (xp <=5000){
+    classificacao = 'Prata';
+} else if (xp <=7000){
+    classificacao = 'Ouro';
+} else if (xp <=8000){
+    classificacao = 'Platina';
+} else if (xp <=9000){
+    classificacao = 'Ascendente';
+} else if (xp <=10000){
+    classificacao = 'Imortal';
+} else{
+    classificacao = 'Radiante';
+} 
+
+console.log("O Herói de nome " + nome + " está no nível " + classificacao);
+
+
+
